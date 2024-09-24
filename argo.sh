9bcedfe4706136c50adeb14328bc164d3a4b942f
Poorna32 <poornachandraga44@gmail.com>
2024-09-24 15:59:33 +0530

    Argo

--- argo.sh
+++ argo.sh
@@ -1,3 +1,4 @@
+if [ "$1" == "install" ]; then
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
@@ -6,3 +7,11 @@ sleep 10
 echo URL - https://$(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1)
 echo Username - admin
 echo Password - $(argocd admin initial-password -n argocd | head -1)
+
+
+fi
+
+if [ "$1" == "jobs" ]; then
+  argocd login $(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1) --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
+
+fi
