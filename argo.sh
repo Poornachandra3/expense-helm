ea4fb27786cc5df00700ce80dce3b1935578e741
Poorna32 <poornachandraga44@gmail.com>
2024-09-25 17:15:14 +0530

    Ingress for frontend.

--- argo.sh
+++ argo.sh
@@ -1,7 +1,7 @@
 if [ "$1" == "install" ]; then
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+#kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
 sleep 10
 echo URL - https://$(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1)
